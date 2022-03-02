980f80a211f74e1d4d152bfa630fee7cacf0bbba
KwadroDev <97489426+KwadroDev@users.noreply.github.com>
2022-03-01 22:54:26 -0400

    website complete now Es

---
+++ src/main.js
@@ -0,0 +1,16 @@
+document.querySelector('.navegation').addEventListener('click', () => {
+    document.querySelector('.collapse').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.Title');
+ScrollReveal().reveal('.Header', { delay: 250 });
+ScrollReveal().reveal('.Collage', { delay: 250 });
+ScrollReveal().reveal('.Mint', { delay: 250 });
+ScrollReveal().reveal('.Story', { delay: 250 });
+ScrollReveal().reveal('.Mint-Details', { delay: 250 });
+ScrollReveal().reveal('.Rarity', { delay: 250 });
+ScrollReveal().reveal('.Roadmap', { delay: 250 });
+ScrollReveal().reveal('.container-roadmap', { delay: 250 });
+ScrollReveal().reveal('.Community', { delay: 250 });
+ScrollReveal().reveal('.Owner', { delay: 250 });
+ScrollReveal().reveal('.Footer', { delay: 250 });
